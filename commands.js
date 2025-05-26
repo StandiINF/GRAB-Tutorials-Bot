7d5ea20cf7c714121ecf088da291b3484e5b9f09
StandiINF <standiinf@gmail.com>
2025-05-26 09:38:18 +0100

    q

--- commands.js
+++ commands.js
@@ -6,6 +6,8 @@ export const commands = [
             {
                 name: "name",
                 description: "The name of the deck",
+                "integration_types": [0,1],
+                "contexts": [0,1,2],
                 type: 3,
                 required: true
             }
