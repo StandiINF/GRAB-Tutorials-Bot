e3b9be8d807a08258a1f97dc2cca393be8f977ca
StandiINF <standiinf@gmail.com>
2025-05-26 10:03:41 +0100

    -

--- commands.js
+++ commands.js
@@ -1,16 +1,15 @@
 export const commands = [
-    {
-        name: "deck",
-        description: "Get a deck's cards",
-        options: [
-            {
-                name: "name",
-                description: "The name of the deck",
-                "integration_types": [0,1],
-                "contexts": [0,1,2],
-                type: 3,
-                required: true
-            }
-        ]
-    },
+  {
+    name: "deck",
+    description: "Get a deck's cards",
+    dm_permission: true,
+    options: [
+      {
+        name: "name",
+        description: "The name of the deck",
+        type: 3,
+        required: true
+      }
+    ]
+  }
 ];
