83e6a7029725ef95a4620a405c2301c603f3ea93
vestria <118200330+vestriaa@users.noreply.github.com>
2025-09-06 22:11:21 +0100

    Revert "a"

--- commands.js
+++ commands.js
@@ -47,13 +47,5 @@ export const commands = [
     ],
     integration_types: [0, 1],
     contexts: [0, 1, 2]
-  },
-
-  {
-    name: "version",
-    description: "Show the latest GRAB version and Build Number!",
-    options: [],
-    integration_types: [0, 1],
-    contexts: [0, 1, 2]
   }
 ];
