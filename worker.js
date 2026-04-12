17421a8cd535344eb3eb4424d6a70943e7af46c8
Samuel Bartlett <standiinf@gmail.com>
2026-04-12 18:12:59 +0100

    hello

--- worker.js
+++ worker.js
@@ -116,8 +116,6 @@ export default {
                                                     color,
                                                     image: {
                                                         url: `https://assets.grab-tutorials.live/${firstCardLink}`,
-                                                        width: 1300,
-                                                        height: 671.3
                                                     },
                                                     description: `*Card 1 of ${cardKeys.length}*${downloadLine}${helpText ? `\n\n*${helpText}*` : ""}`
                                                 }
